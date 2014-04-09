ba2336019b83f364ee5337d4ae1ed98049d9437b
hubert <hubertszostek@gmail.com>
2014-04-09 12:29:59 +0200

    Dziala klasteryzacja i znajdowanie K (

--- polls/Scripts/kmeans.py
+++ polls/Scripts/kmeans.py
@@ -1,18 +1,22 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
-dataToCluster: shape(n_samples,m_dimensions)
+Part of the code cames from the Data Science Blog
+http://datasciencelab.wordpress.com/2014/01/21/selection-of-k-in-k-means-clustering-reloaded/
 """
 def ClusterKMeans(dataToCluster):
 
     results=[]
     fs, Sk = findK(1,dataToCluster)
-    for i in range(2,20):
+    results.append(fs)
+    for i in range(2,19):
         fs, Sk = findK(i,dataToCluster,Sk)
         results.append(fs)
     print results
-    k_means=KMeans(n_clusters=3, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances=True, verbose=0, random_state=None, copy_x=True, n_jobs=1)
+    res_clusters=choose_clusters(results,dataToCluster)
+    k_means=KMeans(n_clusters=res_clusters, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances=True, verbose=0, random_state=None, copy_x=True, n_jobs=1)
     k_means.fit(dataToCluster)
     return k_means.cluster_centers_
 
@@ -26,20 +30,16 @@ def findK( local_K, dataToCluster,Skm1=0):
         k_means.fit(dataToCluster)
         mu=k_means.cluster_centers_
         clusters=find_groups(k_means,dataToCluster,local_K)
-        #print mu
-        #print clusters
-        
-        """
         sum=0   
         for i in range(local_K):
             for c in clusters[i]:
                 sum+=np.linalg.norm(mu[i]-c)**2
-
         Sk=sum
         """
         Sk = sum([np.linalg.norm(mu[i]-c)**2 \
                  for i in range(local_K) for c in clusters[i]])
-        #print Sk
+        """
+        print Sk
         #print "___"
         if local_K == 1:
             fs = 1
@@ -47,9 +47,21 @@ def findK( local_K, dataToCluster,Skm1=0):
             fs = 1
         else:
             fs = Sk/(a(local_K,Nd)*Skm1)
-            #print a(local_K,Nd)
+            print a(local_K,Nd)
             #fs = Sk/(Skm1)
         return fs, Sk   
+
+def choose_clusters(results,X):
+    fig = plt.figure(figsize=(40,10))
+    x = range(1, len(results)+1)
+    ax2 = fig.add_subplot(132)
+    ax2.set_ylim(0, 1.2)
+    ax2.plot(x, results, 'ro-', alpha=0.6)
+    ax2.set_xlabel('Number of clusters K', fontsize=15)
+    ax2.set_ylabel('value of f(K)', fontsize=15) 
+    plt.savefig('detK_N%s.png' % (str(len(X))),bbox_inches='tight', dpi=100)
+    result_K= np.where(results == min(results))[0][0] + 1
+    return result_K
 
 def find_groups(k_means,dataToCluster,local_K):
     clusters={}
